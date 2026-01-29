4af46e7acc2dc1b450848ef3db97c91f756c385b
Samuel-Work <patriciosamuel097@gmail.com>
2026-01-29 10:13:49 -0300

    User create restrict to age greater than 18

--- apps/backend/app/src/services/user.py
+++ apps/backend/app/src/services/user.py
@@ -1,8 +1,12 @@
-from app.src.repositories.user import create_user
+from app.src.repositories.user import repo_create_user
 from app.src.models.user import User
 
 class UserService:
   def __init__(self, db):
     self.db = db
+    
   def create_user(self, user: User) -> User:
-    return create_user(self.db, user)
+    if user.age < 18:
+      raise ValueError("User must be at least 18 years old")
+    return repo_create_user(self.db, user)
+  
