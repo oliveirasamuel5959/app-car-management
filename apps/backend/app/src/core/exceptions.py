aeda7877d4799b5d0fbf7ca3af56ffb3c49115c5
Samuel-Main <patriciosamuel097@gmail.com>
2026-01-29 23:01:02 -0300

    Created validation for vehicle creation

---
+++ apps/backend/app/src/core/exceptions.py
@@ -0,0 +1,8 @@
+class DuplicateVehiclePlateError(Exception):
+    pass
+
+class VehicleNotFoundError(Exception):
+    pass
+
+class UserAlreadyHasVehicleError(Exception):
+    pass
--- apps/backend/app/src/services/vehicle.py
+++ apps/backend/app/src/services/vehicle.py
@@ -1,22 +1,33 @@
+from sqlalchemy.exc import IntegrityError
 from app.src.repositories.user import get_user_by_email
 from app.src.repositories.vehicle import repo_create_vehicle
 from app.src.schemas.vehicle import VehicleCreate
 from app.src.repositories.vehicle import get_vehicle_by_user_id
-
+from app.src.core.exceptions import DuplicateVehiclePlateError
+from app.src.repositories.vehicle import check_duplicate_plate
 class VehicleService:
   def __init__(self, db):
     self.db = db
 
   def create_vehicle(self, vehicle_in: VehicleCreate):
     user = get_user_by_email(self.db, vehicle_in.user.email)
-    vehicle = get_vehicle_by_user_id(self.db, user.id)
     
     if not user:
       raise ValueError("User not found")
     
+    vehicle = get_vehicle_by_user_id(self.db, user.id)
+    
+    duplicate_plate = check_duplicate_plate(self.db, vehicle_in.plate)
+
+    if duplicate_plate is not None:
+      raise DuplicateVehiclePlateError()
+
     if vehicle:
       raise ValueError("User already has a vehicle")
     
-    vehicle = repo_create_vehicle(self.db, user_id=user.id, vehicle_data=vehicle_in.dict(exclude={"user"}))
-    return vehicle
+    try:
+      return repo_create_vehicle(self.db, user_id=user.id, vehicle_data=vehicle_in.dict(exclude={"user"}))
+    except IntegrityError:
+      self.db.rollback()
+      raise DuplicateVehiclePlateError()
   
