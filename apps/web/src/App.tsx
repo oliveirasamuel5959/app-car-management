81468d0b6884c2d19b359624198361be3c30f9b9
Samuel-Work <patriciosamuel097@gmail.com>
2026-03-23 16:39:07 -0300

    Add HomePage route and component for landing page

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -4,6 +4,7 @@ import ProtectedRoute from './components/routing/protected-route';
 import { AuthProvider } from './context/auth-context';
 import { publicRoutes, protectedRoutes } from './routes/routes';
 import LoginPage from './pages/login-page';
+import HomePage from './pages/home-page';
 import SignUpPage from './pages/signup-page';
 import './App.css';
 
@@ -15,12 +16,13 @@ function App() {
           {/* Auth routes without MainLayout */}
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/" element={<HomePage />} />
 
           {/* All other routes with MainLayout */}
           <Route path="*" element={
             <MainLayout>
               <Routes>
-                {publicRoutes.filter(r => r.path !== '/login' && r.path !== '/signup').map((route) => (
+                {publicRoutes.filter(r => r.path !== '/login' && r.path !== '/signup' && r.path !== '/').map((route) => (
                   <Route 
                     key={route.path}
                     path={route.path}
